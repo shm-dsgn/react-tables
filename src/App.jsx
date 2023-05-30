23b53ff7ed9ef49338ad7225eeca8e7abd207e9b
shm-dsgn <77527904+shm-dsgn@users.noreply.github.com>
2023-05-31 00:22:10 +0530

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,13 @@ import './App.css'
 import BasicTable from './components/BasicTable'
 import SortingTable from './components/SortingTable'
 import FilteringTable from './components/FilteringTable'
+import PaginationTable from './components/PaginationTable'
 
 function App() {
 
   return (
     <>
-      <FilteringTable/>
+      <PaginationTable/>
     </>
   )
 }
